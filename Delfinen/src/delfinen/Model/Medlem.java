1d3bb56f5ec2a1e7750604f270cbf1bd4af2973f
Mathias Parking <cph-mp525@cphbusiness.dk>
2019-11-26 12:56:28 +0100

    with model

---
+++ Delfinen/src/delfinen/Model/Medlem.java
@@ -0,0 +1,20 @@
+package delfinen.Model;
+
+
+public class Medlem {
+    private int alder;
+    private String aktivitetsForm;
+    private int kontingentBetaling;
+    private String navn;
+
+    public Medlem(int alder, String aktivitetsForm, int kontingentBetaling, String navn) {
+        this.alder = alder;
+        this.aktivitetsForm = aktivitetsForm;
+        this.kontingentBetaling = kontingentBetaling;
+        this.navn = navn;
+    }
+    
+    
+    
+}
+
